7bc415c307ad0ca596b12b3fd2348e07edc97b55
neilclifton42 <neil.clifton@cliftonwebfoundry.com.au>
2019-04-11 16:10:40 +1000

    tweaking the docker container and checking the request contains to and message

--- send.php
+++ send.php
@@ -8,26 +8,29 @@ $json = file_get_contents('php://input');
 // Get as an object
 $message = json_decode($json);
 
-// prepare and send the message using the BurstSMS API
-try {
-    $service = \SendSMS\Service::message($message->to, $message->message);
-    $sent    = $service->shortenUrls()->send();
+if ($message->to && $message->message){
+    // prepare and send the message using the BurstSMS API
+    try {
+        $service = \SendSMS\Service::message($message->to, $message->message);
+        // if all ok respond with message sent JSON
+        if ($service->shortenUrls()->send()) {
+            $data = ['message' => 'message sent'];
+        } else {
+            $data = $service->getError();
+            http_response_code(400);
 
-    // if all ok respond with message sent JSON
-    if ($sent) {
-        $data = ['message' => 'message sent'];
-    } else {
-        $data = $service->getError();
-        http_response_code(400);
+        }
+        header('Content-Type: application/json');
+        echo json_encode($data);
+
+    } catch (Exception $e) {
+        http_response_code(500);
+        echo json_encode(['error' => $e->getmessage,'request'=>$message]);
 
     }
-    header('Content-Type: application/json');
-    echo json_encode($data);
-
-} catch (Exception $e) {
-    $sent = false;
-    http_response_code(500);
-    echo json_encode(['error' => $e->getmessage]);
+} else {
+    http_response_code(400);
+    echo json_encode(['error' => 'invalid request','request'=>$message]);
 
 }
 
