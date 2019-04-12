fbe4fa936a3170b0ebe22aa2d5db2a01dc969d0c
neilclifton42 <neil.clifton@cliftonwebfoundry.com.au>
2019-04-12 13:41:14 +1000

    fronend javascript to force the to into the correct format, numbers prefixed with specified prefix

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,84 @@
 <template>
-    <div id="app">
-        <h1>Send a message</h1>
-        <form @submit.prevent="onSubmit">
-            <div>
-                <label for="to">To Phone Number: </label>
-                <input id="to" type="tel" v-model="form.to" required pattern="61[0-9]{9}">
-            </div>
-            <div>
-                <label for="message"> Message: </label>
-            </div>
-            <div>
+  <div id="app">
+    <h1>Send a message</h1>
+    <form @submit.prevent="onSubmit">
+      <div>
+        <label for="to">To Phone Number: </label>
+        <input id="to" type="tel" v-model="form.to" required :pattern="toPattern">
+      </div>
+      <div>
+        <label for="message"> Message: </label>
+      </div>
+      <div>
                 <textarea
-                        id="message"
-                        type="text"
-                        v-model="form.message"
-                        :maxlength="765"
-                        rows="5"
-                        cols="40"
-                        required
+                  id="message"
+                  type="text"
+                  v-model="form.message"
+                  :maxlength="765"
+                  rows="5"
+                  cols="40"
+                  required
                 ></textarea>
-            </div>
-            <div>
-                <button type="submit">Send</button>
-            </div>
-            <div>
-                {{ message }}
-            </div>
-        </form>
-    </div>
+      </div>
+      <div>
+        <button type="submit">Send</button>
+      </div>
+      <div>
+        {{ message }}
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-    import axios from 'axios'
+  import axios from 'axios'
 
-    export default {
-        name: 'app',
-        props: {
-
+  export default {
+    name: 'app',
+    props: {
+      toPrefix:{
+        type: String,
+        default: '61'
+      },
+      toPattern:{
+        type: String,
+        default: '61[0-9]{9}'
+      }
+    },
+    data() {
+      return {
+        form: {
+          to: null,
+          message: null
         },
-        data() {
-            return {
-                form: {
-                    to: null,
-                    message: null
-                },
-                message: null
-            }
-        },
-        methods: {
-            onSubmit() {
-                axios
-                    .post('send.php', this.form)
-                    .then(response => this.message = response.data.message)
-                    .catch(error => {console.log(error);this.message='message failed';})
-            }
+        message: null,
+      }
+    },
+    created(){
+      this.form.to = this.toPrefix;
+    },
+    watch: {
+      "form.to": function () {
+        // first characters will always be the to prefix,
+        if (this.form.to.substring(0, this.toPrefix.length) !== this.toPrefix) {
+          this.form.to = this.toPrefix + this.form.to.substring(2);
         }
+        // remove any non-numerics
+        this.form.to = this.form.to.replace(/[^0-9]/g,'');
+      }
+    },
+    methods: {
+      onSubmit() {
+        axios
+          .post('send.php', this.form)
+          .then(response => this.message = response.data.message)
+          .catch(error => {
+            console.log(error);
+            this.message = 'message failed';
+          })
+      },
     }
+  }
 </script>
 
 <style lang="scss">
